d969508cf11508b066204915ed586b7f3d4c373d
tim-ras <46749192+tim-ras@users.noreply.github.com>
2019-01-30 09:11:04 +0100

    Update jv.js

--- jv.js
+++ jv.js
@@ -1,1 +1,16 @@
 // JavaScript source code
+function readTextFile()
+{
+    var openFile = function(event) {
+        var input = event.target;
+
+        var reader = new FileReader();
+        reader.onload = function(){
+          var text = reader.result;
+          var node = document.getElementById('output');
+          node.innerText = text;
+          console.log(reader.result.substring(0, 200));
+        };
+        reader.readAsText(input.files[0]);
+      };
+}
